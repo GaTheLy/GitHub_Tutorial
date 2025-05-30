09eded3f416702cd790bc67365ee73e9bb5433f4
Abelito Faleyrio Visese <afvisese@gmail.com>
2025-05-30 08:39:38 +0700

    Initial Commit

---
+++ GitHub_Tutorial/GitHub_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TutorialApp.swift
+//  GitHub_Tutorial
+//
+//  Created by Abelito Faleyrio Visese on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHub_TutorialTests/GitHub_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TutorialTests.swift
+//  GitHub_TutorialTests
+//
+//  Created by Abelito Faleyrio Visese on 30/05/25.
+//
+
+import Testing
+@testable import GitHub_Tutorial
+
+struct GitHub_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
